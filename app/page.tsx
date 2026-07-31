bf644f2e8fbf8f7b982a0be206b7f4af2f250c5e
apkzoz85-dev <apkzoz85@gmail.com>
2026-07-31 22:34:02 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const WA_MESSAGE = encodeURIComponent(
 const WA_URL = `https://wa.me/${WA_NUMBER}?text=${WA_MESSAGE}`;
 
 // TODO: ضع مفتاح Web3Forms هنا
-const WEB3_KEY = "YOUR_WEB3FORMS_ACCESS_KEY_HERE";
+const WEB3_KEY = "14be81e6-21a3-4887-8efe-69b2cd01061d";
 // ============================================================
 
 function useReveal() {
